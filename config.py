ba0663d156b461f8803ca5bc425cad94dadb1b2a
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-24 12:43:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
+TELEGRAM_BOT_TOKEN = os.getenv("7605997710:AAFEA7cgAmx34sC8MXzHIFOWgy-Gk4UsHCc")
+MONGO_URI = os.getenv("mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ADMIN_CHAT_ID = int(os.getenv("1938030055"))
 PORT = int(os.getenv("PORT", 5000))
 WEB_APP_URL = os.getenv("WEB_APP_URL", f"http://localhost:{PORT}")
