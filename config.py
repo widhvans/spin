c3e1ae8032427399f388627da6665b29be31f048
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-24 12:19:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
-WEB_APP_URL = os.getenv("WEB_APP_URL")  # e.g., https://your-mini-app-domain.com
+WEB_APP_URL = os.getenv("WEB_APP_URL", "http://localhost:5000")  # Updated by Ngrok
