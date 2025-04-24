43d4e06e88eb09d48796117ececb90b444b63aed
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-24 11:47:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
